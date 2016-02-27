0d506fda11315c30dd2712ea764ab8f2b52788b9
andy1729 <andy1729>
2016-02-28 02:40:34 +0530

    Intialised the poll app

---
+++ django101/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    published = models.DateTimeField('Publish Date')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=50)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return choice_text
